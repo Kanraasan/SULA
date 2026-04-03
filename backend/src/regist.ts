f2054cb8148bfe8a7f40c409610f17fab933b835
Yanuar Purnama <yanuarrizkymumu@gmail.com>
2026-04-03 16:46:39 +0700

    Fixing major bugs and adding a new page
    I was fixing a bunch of bugs and added a page for user to edit and check
    their own post. List of bug fixed:
    
    - Login and Register Feature.
    - All Post Feature (ex: post, edit, delete)

--- backend/src/regist.ts
+++ backend/src/regist.ts
@@ -1,14 +1,5 @@
 import { hashSync } from 'bcryptjs';
 
-const regist: Array<any> = [
-  {
-    NIK: 3310012345678901,
-    username: "testuser",
-    password: hashSync("password123", 10),
-    alamatLengkap: "Jl. Test No. 123",
-    kecamatan: "Laweyan",
-    kelurahan: "Bumi",
-  }
-];
+const regist: Array<any> = [];
 
 export default regist;
